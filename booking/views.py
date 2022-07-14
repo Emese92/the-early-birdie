feaccc6491cb1f183067bee3d8f053eb282e24d0
Emese92 <gregor.emese@gmail.com>
2022-07-14 23:09:12 +0000

    Add my stlye and structure to base.html

--- booking/views.py
+++ booking/views.py
@@ -5,11 +5,10 @@ from .models import Booking
 
 class BookingList(generic.ListView):
     model = Booking
-    queryset = Booking.objects.filter(account=account).order_by('-booked_date') 
+    queryset = Booking.objects.order_by('-booked_date') 
     template_name = 'bookings.html'
     paginate_by = 20
 
- 
 
 
 class BookingDetail(View):
@@ -17,7 +16,5 @@ class BookingDetail(View):
     def get(self, request, slug, *args, **kwargs):
         quesryset = Booking.objects.all()
         booking = get_object_or_404(quesryset, slug=slug)
+        template_name = "booking_detail.html"
 
-    return render(
-        request,
-        "booking_detail.html",)
