be08662f68fb8de51fa35af0842dca673ddc34b7
Emese92 <gregor.emese@gmail.com>
2022-07-14 19:42:48 +0000

    Add html and css files from code institute's template

--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Booking
 
 
-class BookingList(genericView):
+class BookingList(generic.ListView):
     model = Booking
-    queryset = Booking.objects.order_by('-booked_date') 
+    queryset = Booking.objects.filter(account=account).order_by('-booked_date') 
     template_name = 'bookings.html'
     paginate_by = 20
+
+ 
+
+
+class BookingDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        quesryset = Booking.objects.all()
+        booking = get_object_or_404(quesryset, slug=slug)
+
+    return render(
+        request,
+        "booking_detail.html",)
