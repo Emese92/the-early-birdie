40143371d4d721bb6951a5e223667a3fed2ec9ce
Emese92 <gregor.emese@gmail.com>
2022-07-14 12:19:28 +0000

    Create superuser and connecting Booking model to it

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+
+@admin.register(Booking)
+
+class BookingAdmin(admin.ModelAdmin):
+
+    list_display = ('booked_date', 'booked_time', 'name', 'party_size', 'approved')
+    search_fields = ['name', 'booked_date']
+    prepopulated_fields = {'slug':('booked_date',)}
+    list_filter = ('booked_on', 'approved')
+
+    def approve_booking(self, request, queryset):
+        queryset.update(approved=True)
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+
+class Booking(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+    booked_date = models.DateField()
+    booked_time = models.TimeField(default=dt.time(00, 00))
+    name = models.CharField(max_length=200)
+    party_size = models.PositiveSmallIntegerField(null=False)
+    extra_info = models.TextField(blank=True)
+    booked_on = models.DateTimeField(auto_now=True)
+    account = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-booked_date']
+
+    def __str__(self):
+        return self.name
+
