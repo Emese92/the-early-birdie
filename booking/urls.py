eecdf3c25176d9a4a6d39fff4ad3016e22c2d5b8
Emese92 <gregor.emese@gmail.com>
2022-07-15 14:53:33 +0000

    Connect index to base.html and add style to it

--- booking/urls.py
+++ booking/urls.py
@@ -1,7 +1,11 @@
 from . import views
 from django.urls import path
+from .views import HomeTemplateView
 
 
 urlpatterns = [
-    path('', views.BookingList.as_view(), name='home')
+    path('', HomeTemplateView.as_view(), name='home'),
+    # path('bookings/', views.BookingList.as_view(), name='booking'),
+    # path('<slug:slug>', views.BookingDetail),
+    
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Booking
+from django.views.generic.base import TemplateView
+
+class HomeTemplateView(TemplateView):
+    template_name = 'index.html'
 
 
-class BookingList(generic.ListView):
-    model = Booking
-    queryset = Booking.objects.order_by('-booked_date') 
-    template_name = 'bookings.html'
-    paginate_by = 20
+# class BookingList(generic.ListView):
+#     model = Booking
+#     queryset = Booking.objects.order_by('-booked_date') 
+#     template_name = 'bookings.html'
+#     paginate_by = 20
 
 
-
-class BookingDetail(View):
-
-    def get(self, request, slug, *args, **kwargs):
-        quesryset = Booking.objects.all()
-        booking = get_object_or_404(quesryset, slug=slug)
-        template_name = "booking_detail.html"
-
+# class BookingDetail(View):
+#     def get(self, request, slug, *args, **kwargs):
+#         quesryset = Booking.objects.all()
+#         booking = get_object_or_404(quesryset, slug=slug)
+#         template_name = "booking_detail.html"
