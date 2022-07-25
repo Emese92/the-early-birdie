31e4df4ebb881a5dff6e82bd389fb78c16be32f5
Emese92 <gregor.emese@gmail.com>
2022-07-25 19:14:09 +0000

    Trying to fix rendering issue for bookings(no success so far)

--- booking/forms.py
+++ booking/forms.py
@@ -3,10 +3,11 @@ from django import forms
 import datetime as dt
 
 
-TIME_CHOICES = [(i, dt.time(i).strftime('%I:%M %p')) for i in range(6,11)]
-PARTY_SIZE = [(i,i) for i in range(1,11)]
+# TIME_CHOICES = [(i, dt.time(i).strftime('%I:%M %p')) for i in range(6, 11)]
+PARTY_SIZE = [(i, i) for i in range(1, 11)]
 
 class BookingForm(forms.ModelForm):
+
     class Meta:
         model = Booking
         fields = [
@@ -16,17 +17,13 @@ class BookingForm(forms.ModelForm):
             'party_size',
             'extra_info',
         ]
-        help_texts = {'extra-info': "Food allergies, dietary requirements, special occasions"}
         widgets = {
             'booked_date': forms.DateInput(
                 format=("%d-%m-%Y"),
                 attrs={'type': 'date'}),
-            'booked_time': forms.Select(choices=TIME_CHOICES),
-            'party_size': forms.Select(choices=PARTY_SIZE),  
+            'booked_time': forms.TimeInput(attrs={'type': 'time'}),
+            'party_size': forms.Select(choices=PARTY_SIZE),
+            'extra_info': forms.widgets.Textarea(
+                attrs={"placeholder": "Food allergies, dietary requirements, special occasions...", })
         }
-
-
-
-
-
 
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
+from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView, FormView
 from .models import Booking
 from django.views.generic.base import TemplateView
 from .forms import BookingForm 
@@ -19,29 +21,49 @@ class BookingList(generic.ListView):
     Booking.objects.values()
 
 
-class AddBooking(generic.ListView):
+# class AddBooking(CreateView):
+#     model = Booking
+#     template_name = 'add_new_booking.html'
+#     fields = [
+#             'booked_date',
+#             'booked_time',
+#             'name',
+#             'party_size',
+#             'extra_info',
+#         ]
+
+#     def get(self, request):
+#         form = BookingForm()
+#         return render(request, 'add_new_booking.html', {"form": form})
+
+
+
+#     def add(request, *args, **kwargs):
+#         if request.method == 'POST':
+#             self.object.create(booked_date=booked_date,
+#                 booked_time=booked_time,
+#                 name=name,
+#                 party_size=party_size,
+#                 extra_info=extra_info,)
+#             return redirect(get)
+
+
+#         return render(request, "add_new_booking.html")
+
+class AddBooking(CreateView):
     model = Booking
+    form = BookingForm()
     template_name = 'add_new_booking.html'
-
-    def get(self, request):
-        return render(request, 'add_new_booking.html', {'form': BookingForm(),})
-
-    def add(request):
-        if request.method == "POST":
-            name = request.POST['name']
-            booked_date = request.POST['booked_date']
-            booked_time = request.POST['booked_time']
-            party_size = request.POST['party_size']
-            extra_info = request.POST['extra_info']
-            
-        
-        return render(request, 'bookings.html',{
-            'name':name,
-            'booked_date':booked_date,
-            'booked_time':booked_time,
-            'party_size':party_size,
-            'extra_info':extra_info,
-            })
-
     
+    def get(self, request):
+        form = BookingForm()
+        return render(request, 'add_new_booking.html', {"form": form})
+
+def post(self, request, *args, **kwargs):
+    form = BookingForm(request.POST or None)
+    if form.is_valid():
+        booking = BookingForm.save(commit=False)
+        booking.save()
+
+    return render(request)
 
