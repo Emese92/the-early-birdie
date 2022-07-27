78eb05e8f9b6acb3baff9ec7bed033e2161c5af5
Emese92 <gregor.emese@gmail.com>
2022-07-27 00:23:35 +0000

    Create option for deleting bookings

--- booking/urls.py
+++ booking/urls.py
@@ -1,11 +1,15 @@
 from . import views
 from django.urls import path
-from .views import HomeTemplateView, MenuTemplateView, BookingList, AddBooking
+from .views import HomeTemplateView, MenuTemplateView, BookingList, AddBooking, deleteBooking
+
+
 
 urlpatterns = [
     path('', HomeTemplateView.as_view(), name='home'),
     path('menu/', MenuTemplateView.as_view(), name='menu'),
     path('bookings/', views.BookingList.as_view(), name='bookings'),
     path('add_new_booking/', AddBooking.as_view(), name='add_new_booking'),
+    # path('edit/<slug:slug>/', views.updateView, name='edit'),
+    path('delete_booking/<str:pk>/', views.deleteBooking, name='delete'),
 
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import generic, View
 from django.http import HttpResponseRedirect
-from django.views.generic.edit import CreateView, FormView
+from django.views.generic.edit import CreateView, FormView, UpdateView
 from .models import Booking
 from django.views.generic.base import TemplateView
 from .forms import BookingForm 
@@ -30,7 +30,7 @@ class AddBooking(TemplateView):
     
     def get(self, request, *args, **kwargs):
         form = BookingForm()
-        context = {"form": form, "booked": False, "confirmed": False,}
+        context = {"form": form, "booked": False, "confirmed": False, "deleted": False, }
         return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
@@ -40,6 +40,61 @@ class AddBooking(TemplateView):
             form.instance.account = request.user
             form.save()
         
-        context = {"form": form, "booked": True, "confirmed": False,}
+        context = {"form": form, "booked": True, "confirmed": False, "deleted": False,}
         return render(request, self.template_name, context)
 
+
+def updateView(request):
+
+    obj = get_object_or_404(Booking, slug=slug, user=request.user)
+    form = BookingForm(request.POST, instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {"form": form, "booked": True, "confirmed": False, }
+    return render(request, "edit_booking.html", context)
+
+
+def deleteBooking(request, pk=None):
+
+    booking = get_object_or_404(Booking, pk=pk)
+    booking.delete()
+    messages.success(request, "Booking Deleted")
+    context = {"deleted": True,}
+    return render(request, "delete.html", context)
+
+# class UpdateBooking(UpdateView):
+#     model = Booking
+#     template_name = 'edit_booking.html'
+#     fields = [
+#             'booked_date',
+#             'booked_time',
+#             'name',
+#             'party_size',
+#             'extra_info',
+#         ]
+    # def edit(self, request, pk):
+    #     booking = get_object_or_404(Booking)
+    #     form = BookingForm(instance=booking)
+
+    #     if request.method == POST:
+    #         form = BookingForm(request.POST, instance=booking)
+    #         if form.is_valid():
+    #             form.instance.account = request.user
+    #             form.save()
+            
+    #     context = {"form": form, "booked": True, "confirmed": False, }
+    #     return render(request, self.template_name, context)
+
+# def updateBooking(request, pk):
+#     template_name = 'edit_booking.html'
+#     booking = Booking.objects.get(id=pk)
+#     form = BookingForm(instance=booking)
+#     context = {"form": form}
+#     return render(request, 'edit_booking.html', context)
+
+# def deleteBooking(request):
+
+#     form = BookingForm()
+#     if request.method == POST:
+#         booking.delete()
+#     return redirect('bookings.html')
